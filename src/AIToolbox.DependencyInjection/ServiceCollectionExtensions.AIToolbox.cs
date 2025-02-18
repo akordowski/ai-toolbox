c3b99c023911b47d31de97a63bb2b0811616f0f2
Artur Kordowski <9746197+akordowski@users.noreply.github.com>
2025-02-18 09:40:58 +0100

    Add dependency injection extension methods for AIToolbox, add unit tests

---
+++ src/AIToolbox.DependencyInjection/ServiceCollectionExtensions.AIToolbox.cs
@@ -0,0 +1,75 @@
+using AIToolbox.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AIToolbox;
+
+public static partial class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Adds the AIToolbox to the <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+    /// <param name="builderAction">A delegate that is used to configure an <see cref="IAIToolboxBuilder"/>.</param>
+    /// <returns>The value of <paramref name="services"/>.</returns>
+    /// <exception cref="ArgumentNullException">Any of the arguments is <see langword="null"/>.</exception>
+    public static IServiceCollection AddAIToolbox(
+        this IServiceCollection services,
+        Action<IAIToolboxBuilder> builderAction)
+    {
+        ArgumentNullException.ThrowIfNull(services, nameof(services));
+        ArgumentNullException.ThrowIfNull(builderAction, nameof(builderAction));
+
+        return services.AddAIToolbox(builderAction, _ => { });
+    }
+
+    /// <summary>
+    /// Adds the AIToolbox to the <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+    /// <param name="builderAction">A delegate that is used to configure an <see cref="IAIToolboxBuilder"/>.</param>
+    /// <param name="optionsAction">A delegate that is used to configure the <see cref="AIToolboxOptions"/>.</param>
+    /// <returns>The value of <paramref name="services"/>.</returns>
+    /// <exception cref="ArgumentNullException">Any of the arguments is <see langword="null"/>.</exception>
+    public static IServiceCollection AddAIToolbox(
+        this IServiceCollection services,
+        Action<IAIToolboxBuilder> builderAction,
+        Action<AIToolboxOptions> optionsAction)
+    {
+        ArgumentNullException.ThrowIfNull(services, nameof(services));
+        ArgumentNullException.ThrowIfNull(builderAction, nameof(builderAction));
+        ArgumentNullException.ThrowIfNull(optionsAction, nameof(optionsAction));
+
+        var options = new AIToolboxOptions();
+        optionsAction.Invoke(options);
+
+        var builder = new AIToolboxBuilder(options, services);
+        builderAction.Invoke(builder);
+
+        return services;
+    }
+
+    /// <summary>
+    /// Adds the AIToolbox to the <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+    /// <param name="builderAction">A delegate that is used to configure an <see cref="IAIToolboxBuilder"/>.</param>
+    /// <param name="configuration">The configuration being bound.</param>
+    /// <param name="sectionName">The name of the configuration section that contains <see cref="AIToolboxOptions"/>.</param>
+    /// <returns>The value of <paramref name="services"/>.</returns>
+    /// <exception cref="ArgumentNullException">Any of the arguments is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentException"><paramref name="sectionName"/> is empty or whitespace.</exception>
+    public static IServiceCollection AddAIToolbox(
+        this IServiceCollection services,
+        Action<IAIToolboxBuilder> builderAction,
+        IConfiguration configuration,
+        string sectionName = "AIToolbox")
+    {
+        ArgumentNullException.ThrowIfNull(services, nameof(services));
+        ArgumentNullException.ThrowIfNull(builderAction, nameof(builderAction));
+        ArgumentNullException.ThrowIfNull(configuration, nameof(configuration));
+        ArgumentException.ThrowIfNullOrWhiteSpace(sectionName, nameof(sectionName));
+
+        return services.AddAIToolbox(builderAction, options => configuration.GetRequiredSection(sectionName).Bind(options));
+    }
+}
---
+++ tests/AIToolbox.DependencyInjection.UnitTests/DependencyInjection/ServiceCollectionExtensions.AIToolbox.Tests.cs
@@ -0,0 +1,142 @@
+using AIToolbox.Options;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
+// ReSharper disable ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
+
+namespace AIToolbox.DependencyInjection;
+
+public class ServiceCollectionExtensionsAIToolboxTests
+{
+    private static readonly Type TypeArgumentException = typeof(ArgumentException);
+    private static readonly Type TypeArgumentNullException = typeof(ArgumentNullException);
+
+    private readonly ServiceCollection _services = [];
+    private readonly Mock<Action<IAIToolboxBuilder>> _builderActionMock = new();
+    private readonly Mock<Action<AIToolboxOptions>> _optionsActionMock = new();
+
+    public static TheoryData<Action> AddAIToolboxWithNullServices
+    {
+        get
+        {
+            ServiceCollection services = null!;
+
+            return
+            [
+                () => services.AddAIToolbox(_ => { }),
+                () => services.AddAIToolbox(_ => { }, _ => { }),
+                () => services.AddAIToolbox(_ => { }, Mock.Of<IConfiguration>())
+            ];
+        }
+    }
+
+    public static TheoryData<Action, string, Type> AddAIToolboxWithInvalidParameters
+    {
+        get
+        {
+            Action<IAIToolboxBuilder> nullBuilderAction = null!;
+            Action<AIToolboxOptions> nullOptionsAction = null!;
+            IConfiguration nullConfiguration = null!;
+
+            var services = new ServiceCollection();
+            var configuration = Mock.Of<IConfiguration>();
+
+            return new TheoryData<Action, string, Type>
+            {
+                { () => services.AddAIToolbox(nullBuilderAction), "builderAction", TypeArgumentNullException },
+
+                { () => services.AddAIToolbox(nullBuilderAction, _ => { }), "builderAction", TypeArgumentNullException },
+                { () => services.AddAIToolbox(_ => { }, nullOptionsAction), "optionsAction", TypeArgumentNullException },
+
+                { () => services.AddAIToolbox(nullBuilderAction, configuration), "builderAction", TypeArgumentNullException },
+                { () => services.AddAIToolbox(_ => { }, nullConfiguration), "configuration", TypeArgumentNullException },
+                { () => services.AddAIToolbox(_ => { }, configuration, null!), "sectionName", TypeArgumentNullException },
+                { () => services.AddAIToolbox(_ => { }, configuration, ""), "sectionName", TypeArgumentException },
+                { () => services.AddAIToolbox(_ => { }, configuration, " "), "sectionName", TypeArgumentException }
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(AddAIToolboxWithNullServices))]
+    public void Should_Throw_On_Add_AIToolbox_With_Null_Services(Action act)
+    {
+        // Assert
+        act.Should().Throw<ArgumentNullException>().WithParameterName("services");
+    }
+
+    [Theory]
+    [MemberData(nameof(AddAIToolboxWithInvalidParameters))]
+    public void Should_Throw_On_Add_AIToolbox_With_Invalid_Parameters(Action act, string parameterName, Type exceptionType)
+    {
+        // Assert
+        if (exceptionType == TypeArgumentException)
+        {
+            act.Should().Throw<ArgumentException>().WithParameterName(parameterName);
+        }
+        else if (exceptionType == TypeArgumentNullException)
+        {
+            act.Should().Throw<ArgumentNullException>().WithParameterName(parameterName);
+        }
+    }
+
+    [Fact]
+    public void Should_Add_AIToolbox_With_Default_Options()
+    {
+        // Act
+        _services.AddAIToolbox(_builderActionMock.Object);
+
+        // Assert
+        _builderActionMock.Verify(action => action(It.IsAny<IAIToolboxBuilder>()), Times.Once);
+    }
+
+    [Fact]
+    public void Should_Add_AIToolbox_With_Options_Action()
+    {
+        // Act
+        _services.AddAIToolbox(_builderActionMock.Object, _optionsActionMock.Object);
+
+        // Assert
+        _builderActionMock.Verify(action => action(It.IsAny<IAIToolboxBuilder>()), Times.Once);
+        _optionsActionMock.Verify(action => action(It.IsAny<AIToolboxOptions>()), Times.Once);
+    }
+
+    [Fact]
+    public void Should_Add_AIToolbox_With_Configuration_And_Default_Section_Name()
+    {
+        // Arrange
+        var configuration = GetConfiguration("AIToolbox");
+
+        // Act
+        _services.AddAIToolbox(_builderActionMock.Object, configuration);
+
+        // Assert
+        _builderActionMock.Verify(action => action(It.IsAny<IAIToolboxBuilder>()), Times.Once);
+        _builderActionMock.Verify(action => action(It.Is<IAIToolboxBuilder>(builder => builder.Options != null)), Times.Once);
+    }
+
+    [Fact]
+    public void Should_Add_AIToolbox_With_Configuration_And_Custom_Section_Name()
+    {
+        // Arrange
+        const string sectionName = "ConfigSection";
+        var configuration = GetConfiguration(sectionName);
+
+        // Act
+        _services.AddAIToolbox(_builderActionMock.Object, configuration, sectionName);
+
+        // Assert
+        _builderActionMock.Verify(action => action(It.IsAny<IAIToolboxBuilder>()), Times.Once);
+        _builderActionMock.Verify(action => action(It.Is<IAIToolboxBuilder>(builder => builder.Options != null)), Times.Once);
+    }
+
+    private static IConfiguration GetConfiguration(string section) =>
+        new ConfigurationBuilder()
+            .AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                [$"{section}:Options"] = null
+            }!)
+            .Build();
+}
