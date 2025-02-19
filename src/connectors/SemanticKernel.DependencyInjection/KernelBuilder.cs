5c8bb5940082eeaa91c951098a6f19f3ff04e834
Artur Kordowski <9746197+akordowski@users.noreply.github.com>
2025-02-19 11:20:30 +0100

    Register KernelProvider in KernelBuilder

--- src/connectors/SemanticKernel.DependencyInjection/KernelBuilder.cs
+++ src/connectors/SemanticKernel.DependencyInjection/KernelBuilder.cs
@@ -1,4 +1,5 @@
 using AIToolbox.Options.SemanticKernel;
+using AIToolbox.SemanticKernel;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AIToolbox.DependencyInjection;
@@ -19,6 +20,8 @@ internal sealed class KernelBuilder : IKernelBuilder
         Options = options;
         Services = services;
 
-        services.AddSingleton(options);
+        services
+            .AddSingleton(options)
+            .AddSingleton<IKernelProvider, KernelProvider>();
     }
 }
