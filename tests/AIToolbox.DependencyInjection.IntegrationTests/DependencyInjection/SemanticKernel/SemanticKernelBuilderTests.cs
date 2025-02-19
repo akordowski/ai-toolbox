2b009a02e7fa984246eefc180fc9382eed033c9f
Artur Kordowski <9746197+akordowski@users.noreply.github.com>
2025-02-19 11:49:01 +0100

    Add SemanticKernelBuilder dependency injection integration tests

---
+++ tests/AIToolbox.DependencyInjection.IntegrationTests/DependencyInjection/SemanticKernel/SemanticKernelBuilderTests.cs
@@ -0,0 +1,95 @@
+using AIToolbox.Options.SemanticKernel;
+using AIToolbox.SemanticKernel;
+using AIToolbox.Tests;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit.DependencyInjection;
+
+namespace AIToolbox.DependencyInjection.SemanticKernel;
+
+public class SemanticKernelBuilderTests : BaseTestWithFixture<AIToolboxFixture>
+{
+    private readonly KernelOptions _kernelOptions = new();
+
+    public SemanticKernelBuilderTests(AIToolboxFixture fixture)
+        : base(fixture)
+    {
+    }
+
+    [Fact]
+    public void Should_Get_Services_Configured_By_Default_Options()
+    {
+        var host = Fixture.GetHost((_, services) =>
+        {
+            services.AddAIToolbox(
+                aiToolbox =>
+                {
+                    aiToolbox.AddSemanticKernel(semanticKernel =>
+                    {
+                        semanticKernel.AddKernel();
+                    });
+                },
+                options =>
+                {
+                    options.SemanticKernel = new SemanticKernelOptions
+                    {
+                        Kernel = _kernelOptions
+                    };
+                });
+        });
+
+        // Assert
+        AssertServices(host.Services);
+    }
+
+    [Fact]
+    public void Should_Get_Services_Configured_By_Options_Action()
+    {
+        var host = Fixture.GetHost((_, services) =>
+        {
+            services.AddAIToolbox(aiToolbox =>
+            {
+                aiToolbox.AddSemanticKernel(
+                    semanticKernel =>
+                    {
+                        semanticKernel.AddKernel();
+                    },
+                    options =>
+                    {
+                        options.Kernel = _kernelOptions;
+                    });
+            });
+        });
+
+        // Assert
+        AssertServices(host.Services);
+    }
+
+    [Fact]
+    public void Should_Get_Services_Configured_By_Config_File()
+    {
+        var host = Fixture.GetHost(
+            (context, services) =>
+            {
+                services.AddAIToolbox(
+                    aiToolbox =>
+                    {
+                        aiToolbox.AddSemanticKernel(semanticKernel =>
+                        {
+                            semanticKernel.AddKernel();
+                        });
+                    },
+                    context.Configuration);
+            },
+            "ConfigSemanticKernelBuilderTests.json");
+
+        // Assert
+        AssertServices(host.Services);
+    }
+
+    private void AssertServices(IServiceProvider services)
+    {
+        services.GetService<KernelOptions>().Should().Be(_kernelOptions);
+        services.GetService<IKernelProvider>().Should().NotBeNull();
+    }
+}
